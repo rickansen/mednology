dd3fd24c3953253ee962a6f9b8918f83c25d7181
Andersen <rickansenn@gmail.com>
2022-09-15 23:51:17 +0800

    Completed reminder page

--- src/routes/Reminder/index.js
+++ src/routes/Reminder/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
-//import styles from './reminder.module.css';
+import styles from './reminder.module.css';
 
+import Water from '../../assets/water.webp';
 import Header from '../../component/Header/header';
+import Menu from '../../component/Menu/menu';
 
 const Reminder = () => {
   return (
-    <>
+    <div className={styles.body}>
       <Header icon="back">REMINDER</Header>
-      <h1>this is reminder</h1>
-    </>
+      <div className={styles.container}>
+        <img src={Water} className={styles.img} alt="benefits of water" />
+
+        <div className={styles.buttonContainer}>
+          <button className={styles.btn}>Add meds</button>
+          <button className={styles.btn}>Edit Meds</button>
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <img src={Water} className={styles.img} alt="benefits of water" />
+
+        <button className={`${styles.btn} ${styles.btn2}`}>Set reminder</button>
+      </div>
+
+      <Menu />
+    </div>
   );
 };
 
