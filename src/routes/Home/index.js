764b650109eb32f30e3b49affbb90c2aeedd02e8
Andersen <rickansenn@gmail.com>
2022-09-15 16:37:31 +0800

    Updated bottom padding to make space for botnav

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -15,63 +15,65 @@ import { ReactComponent as RightArrow } from '../../assets/right-arrow.svg';
 const Home = () => {
   return (
     <>
-      <Header icon="menu">MEDNOLOGY</Header>
-      <Menu />
-      <div className={styles.address}>
-        <span>Deliver to 123 House, Street, City</span>
-        <Next />
-      </div>
-      <form>
-        <input className={styles.input} />
-        <Search className={styles.search} />
-      </form>
-      <img
-        className={styles.promoPmat}
-        src={PromoPmat}
-        alt="Promo Pubmat"
-      ></img>
-      <div>
-        <div className={styles.container}>
-          <Features icon="location" link="/NearbyStores">
-            Nearby Stores
-          </Features>
-          <Features icon="release" link="/NewRelease">
-            New Release
-          </Features>
-          <Features icon="membership" link="/Membership">
-            Membership
-          </Features>
-          <Features icon="promos" link="/Promos">
-            Promos
-          </Features>
+      <div className={styles.body}>
+        <Header icon="menu">MEDNOLOGY</Header>
+        <Menu />
+        <div className={styles.address}>
+          <span>Deliver to 123 House, Street, City</span>
+          <Next />
+        </div>
+        <form>
+          <input className={styles.input} />
+          <Search className={styles.search} />
+        </form>
+        <img
+          className={styles.promoPmat}
+          src={PromoPmat}
+          alt="Promo Pubmat"
+        ></img>
+        <div>
+          <div className={styles.container}>
+            <Features icon="location" link="/NearbyStores">
+              Nearby Stores
+            </Features>
+            <Features icon="release" link="/NewRelease">
+              New Release
+            </Features>
+            <Features icon="membership" link="/Membership">
+              Membership
+            </Features>
+            <Features icon="promos" link="/Promos">
+              Promos
+            </Features>
+          </div>
+
+          <div className={styles.container}>
+            <Features icon="teleconsult" link="/Teleconsultation">
+              Tele Consultation
+            </Features>
+            <Features icon="remind" link="/Reminder">
+              Reminders
+            </Features>
+            <Features icon="convert" link="/Converter">
+              Brand - Generic
+            </Features>
+            <Features icon="others" link="/Others">
+              Others
+            </Features>
+          </div>
         </div>
 
-        <div className={styles.container}>
-          <Features icon="teleconsult" link="/Teleconsultation">
-            Tele Consultation
-          </Features>
-          <Features icon="remind" link="/Reminder">
-            Reminders
-          </Features>
-          <Features icon="convert" link="/Converter">
-            Brand - Generic
-          </Features>
-          <Features icon="others" link="/Others">
-            Others
-          </Features>
-        </div>
-      </div>
+        <div className={styles.newRelease}>
+          <h2 className={styles.header}>NEW RELEASES</h2>
+          <div className={styles.placeholderContainer}>
+            <div className={styles.placeholder}></div>
+            <div className={styles.placeholder}></div>
+            <div className={styles.placeholder}></div>
 
-      <div className={styles.newRelease}>
-        <h2 className={styles.header}>NEW RELEASES</h2>
-        <div className={styles.placeholderContainer}>
-          <div className={styles.placeholder}></div>
-          <div className={styles.placeholder}></div>
-          <div className={styles.placeholder}></div>
-
-          <Link to="/NewRelease">
-            <RightArrow />
-          </Link>
+            <Link to="/NewRelease">
+              <RightArrow />
+            </Link>
+          </div>
         </div>
       </div>
     </>
--- src/routes/NearbyStores/index.js
+++ src/routes/NearbyStores/index.js
@@ -12,44 +12,46 @@ import Location from '../../assets/location-search.svg';
 const NearbyStores = () => {
   return (
     <>
-      <Header icon="back">NEARBY STORES</Header>
-      <Menu />
+      <div className={styles.body}>
+        <Header icon="back">NEARBY STORES</Header>
+        <Menu />
 
-      <div className={styles.container}>
-        <h1 className={styles.header}>FIND A STORE NEAR YOU</h1>
-        <div>
-          <form>
-            <img src={Location} className={styles.pin} alt="search icon" />
-            <input className={styles.input} />
-          </form>{' '}
-          <div className={styles.buttonContainer}>
-            <select className={styles.button}>
-              <option>5km away</option>
-              <option>10km away</option>
-              <option>20km away</option>
-            </select>
-            <button className={`${styles.button} ${styles.black}`}>
-              Apply
-            </button>
+        <div className={styles.container}>
+          <h1 className={styles.header}>FIND A STORE NEAR YOU</h1>
+          <div>
+            <form>
+              <img src={Location} className={styles.pin} alt="search icon" />
+              <input className={styles.input} />
+            </form>{' '}
+            <div className={styles.buttonContainer}>
+              <select className={styles.button}>
+                <option>5km away</option>
+                <option>10km away</option>
+                <option>20km away</option>
+              </select>
+              <button className={`${styles.button} ${styles.black}`}>
+                Apply
+              </button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <Address
-        title="MERCURY DRUG"
-        address="Earnshaw St. cor. San Anton St., Sampaloc, Manila, 1008 Metro Manila"
-        number="(02) 8734-6674 / (02) 8740-8065 / (0908) 813-7153"
-      ></Address>
-      <Address
-        title="MERCURY DRUG"
-        address="Earnshaw St. cor. San Anton St., Sampaloc, Manila, 1008 Metro Manila"
-        number="(02) 8734-6674 / (02) 8740-8065 / (0908) 813-7153"
-      ></Address>
-      <Address
-        title="MERCURY DRUG"
-        address="Earnshaw St. cor. San Anton St., Sampaloc, Manila, 1008 Metro Manila"
-        number="(02) 8734-6674 / (02) 8740-8065 / (0908) 813-7153"
-      ></Address>
+        <Address
+          title="MERCURY DRUG"
+          address="Earnshaw St. cor. San Anton St., Sampaloc, Manila, 1008 Metro Manila"
+          number="(02) 8734-6674 / (02) 8740-8065 / (0908) 813-7153"
+        ></Address>
+        <Address
+          title="MERCURY DRUG"
+          address="Earnshaw St. cor. San Anton St., Sampaloc, Manila, 1008 Metro Manila"
+          number="(02) 8734-6674 / (02) 8740-8065 / (0908) 813-7153"
+        ></Address>
+        <Address
+          title="MERCURY DRUG"
+          address="Earnshaw St. cor. San Anton St., Sampaloc, Manila, 1008 Metro Manila"
+          number="(02) 8734-6674 / (02) 8740-8065 / (0908) 813-7153"
+        ></Address>
+      </div>
     </>
   );
 };
