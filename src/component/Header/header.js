760200386b1e3026a8cc1a9d10a590d44e17cfa2
Andersen <rickansenn@gmail.com>
2022-09-15 11:57:33 +0800

    Back button functionality

--- src/component/Header/header.js
+++ src/component/Header/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './header.module.css';
 
+import { Link } from 'react-router-dom';
 import { ReactComponent as Menu } from '../../assets/hamburger-menu.svg';
 import { ReactComponent as Back } from '../../assets/back.svg';
 
@@ -8,8 +9,15 @@ const Header = ({ icon, children }) => {
   return (
     <>
       <div className={styles.header}>
-        {icon === 'menu' ? <Menu /> : <Back />}
+        {icon === 'menu' ? (
+          <Menu />
+        ) : (
+          <Link to="/Home">
+            <Back />
+          </Link>
+        )}
         <h1>{children}</h1>
+
         <Menu />
       </div>
     </>
