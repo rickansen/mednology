10d3351d7994fa0b20b063dc20a58d35fae80ad1
Andersen <rickansenn@gmail.com>
2022-09-16 09:59:34 +0800

    Refactored promos page -- added pics

--- src/routes/Promos/Comp/comp.js
+++ src/routes/Promos/Comp/comp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './comp.module.css';
 
-const Comp = ({ title, desc, benefits, img }) => {
+const Comp = ({ title, img }) => {
   return (
     <>
       <div className={styles.body}>
@@ -10,8 +10,6 @@ const Comp = ({ title, desc, benefits, img }) => {
 
           <div className={styles.descContainer}>
             <h2 className={styles.title}>{title}</h2>
-            <p>{desc}</p>
-            <span className={styles.benefits}>{benefits}</span>
           </div>
         </div>
       </div>
--- src/routes/Promos/index.js
+++ src/routes/Promos/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from './promos.module.css';
 
 import PromoPic from '../../assets/promo-pic1.webp';
+import PromoPic2 from '../../assets/promo-pic2.webp';
+import PromoPic3 from '../../assets/promo-pic3.webp';
+import PromoPic4 from '../../assets/promo-pic4.webp';
+import PromoPic5 from '../../assets/promo-pic5.webp';
 
 import Header from '../../component/Header/header';
 import Menu from '../../component/Menu/menu';
@@ -12,20 +16,21 @@ const Promos = () => {
     <>
       <div className={styles.body}>
         <Header icon="back">Promos</Header>
-        <h1 className={styles.header}>Latest Promos</h1>
         <Menu />
-        <Comp
-          title="Promo Title"
-          desc="Available promo date range"
-          benefits={'Benefits...'}
-          img={PromoPic}
-        />
-        <Comp
-          title="Promo Title"
-          desc="Available promo date range"
-          benefits={'Benefits...'}
-          img={PromoPic}
-        />
+        <img
+          src={PromoPic}
+          className={styles.banner}
+          alt="Promo page banner"
+        />{' '}
+        <h1 className={styles.header}>Latest Promos</h1>
+        <div className={styles.compContainer}>
+          <Comp title="Free delivery" img={PromoPic2} />
+          <Comp title="Get Souvenirs" img={PromoPic3} />
+        </div>
+        <div className={styles.compContainer}>
+          <Comp title="Buy 2 Get 1" img={PromoPic4} />
+          <Comp title="Buy 1 Get 1" img={PromoPic5} />
+        </div>
       </div>
     </>
   );
