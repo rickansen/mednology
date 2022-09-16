42c5bdea083ddf5d5591a22c2c67ce8e1c5f5061
Andersen <rickansenn@gmail.com>
2022-09-16 23:14:07 +0800

    made pics in option page a link

--- src/routes/Option/index.js
+++ src/routes/Option/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './option.module.css';
 
+import { Link } from 'react-router-dom';
+
 import ForMe from '../../assets/for-me.webp';
 import ForMyPet from '../../assets/for-my-pet.webp';
 import Button from '../../component/Button/button';
@@ -16,28 +18,30 @@ const Option = () => {
 
       <h1 className={styles.q}>Browsing for ..?</h1>
 
-      <div className={styles.flex}>
-        <div className={styles.option}>
-          <div className={styles.picContainer}>
-            <img
-              src={ForMe}
-              className={styles.img}
-              alt="Medicine for humans option"
-            />
-            <span>Me</span>
+      <Link to="/Home" className={styles.link}>
+        <div className={styles.flex}>
+          <div className={styles.option}>
+            <div className={styles.picContainer}>
+              <img
+                src={ForMe}
+                className={styles.img}
+                alt="Medicine for humans option"
+              />
+              <span>Me</span>
+            </div>
+            <div className={styles.picContainer}>
+              <img
+                src={ForMyPet}
+                className={styles.img}
+                alt="Medicine for pet option"
+              />
+              <span>My Pet</span>
+            </div>
           </div>
-          <div className={styles.picContainer}>
-            <img
-              src={ForMyPet}
-              className={styles.img}
-              alt="Medicine for pet option"
-            />
-            <span>My Pet</span>
-          </div>
-        </div>
 
-        <Button link="/home">CONTINUE</Button>
-      </div>
+          <Button link="/home">CONTINUE</Button>
+        </div>
+      </Link>
     </>
   );
 };
